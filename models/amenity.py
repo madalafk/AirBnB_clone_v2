82be701e4ecda4908db54b3f216284673319d7b2
fmadala@hotmail.com <fmadala@hotmail.com>
2023-08-23 12:18:42 -0700

    updated amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" definitions for amenity class
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity for my database
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    __table_args__ = ({'mysql_default_charset': 'latin1'})
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
