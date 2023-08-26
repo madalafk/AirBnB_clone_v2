c5a580a54eaba68817948ea3fcf81e548f1d25f3
fmadala@hotmail.com <fmadala@hotmail.com>
2023-08-26 11:56:41 -0700

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-""" definitions for amenity class
-"""
+""" definitions for amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.place import place_amenity
 
-
 class Amenity(BaseModel, Base):
-    """ Amenity for my database
+    """This is the class for Amenity
     Attributes:
         name: input name
     """
     __tablename__ = "amenities"
-    __table_args__ = ({'mysql_default_charset': 'latin1'})
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
