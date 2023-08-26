43ec6d27d1749893d4b1dca1d19825315986f3e9
fmadala@hotmail.com <fmadala@hotmail.com>
2023-08-26 13:53:54 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""definition for place class"""
+"""place class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
