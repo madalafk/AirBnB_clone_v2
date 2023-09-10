f3d46eab5ae1f3de56ad7790147d1a72f6c88a03
fmadala@hotmail.com <fmadala@hotmail.com>
2023-09-10 09:24:05 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-'''this generates .tgz archive using fabric'''
+"""this generates .tgz archive using fabric"""
 
 from fabric.api import local
 from datetime import datetime
 
 from fabric.decorators import runs_once
 
-
 @runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
+    """generates .tgz archive from the contents of the web_static folder"""
     local("mkdir -p versions")
     path = ("versions/web_static_{}.tgz"
             .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
@@ -23,7 +22,6 @@ def do_pack():
 """Generates a .tgz archive from the contents of the web_static folder."""
 from fabric.api import local
 import time
-
 
 def do_pack():
     """Generate an tgz archive from web_static folder"""
