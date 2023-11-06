6b0dc22dd4ef5cf08ff303f84f44bcb3cf5144bd
madalafk <fmadala@hotmail.com>
2023-11-06 08:39:26 -0800

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-# generates a .tgz archive from the contents of the web_static folder
-# this uses the function do_pack
+"""
+A script to generate a .tgz archive using Fabric.
+"""
+import os
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
-    """
-    Creates archive from web_static directory
-    """
-    local("mkdir -p versions")
-    file = 'versions/web_static_{}.tgz'\
-        .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
-    comp = 'tar -cvzf {} web_static'.format(file)
-    tar_file = local(comp)
-    if tar_file.failed:
+    """ Generates a .tgz archive from the contents of web_static folder. """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+        return output
+    except Exception:
         return None
-    return file
