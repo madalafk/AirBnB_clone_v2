d4dab445a33b6a0e3c28483c5db530fca92af4a0
madalafk <fmadala@hotmail.com>
2023-11-06 10:43:00 -0800

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,91 +1,100 @@
 # Configures a web server for deployment of web_static.
 
-# Nginx configuration file
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://www.bbc.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
 package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-} ->
+  ensure   => 'installed',
+}
 
 file { '/data':
-  ensure  => 'directory'
-} ->
+  ensure  => directory,
+}
 
 file { '/data/web_static':
-  ensure => 'directory'
-} ->
+  ensure => directory,
+}
 
 file { '/data/web_static/releases':
-  ensure => 'directory'
-} ->
+  ensure => directory,
+}
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory'
-} ->
+  ensure => directory,
+}
 
 file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
+  ensure => directory,
+}
 
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "welcome to my test page Puppet\n"
-} ->
+  ensure  => present,
+  content => "<html>
+	  <head>
+	  </head>
+	< body>
+		  Holberton School
+	  </body>
+</html>",
+}
 
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test'
-} ->
+  target => '/data/web_static/releases/test',
+}
 
 exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+  path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
 
 file { '/var/www':
-  ensure => 'directory'
-} ->
+  ensure => directory,
+}
 
 file { '/var/www/html':
-  ensure => 'directory'
-} ->
+  ensure => directory,
+}
+
+file { '/var/www/error':
+  ensure => directory,
+}
 
 file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "welcome to my test page Nginx\n"
-} ->
+  ensure  => present,
+  content => "Hello World!\n",
+}
 
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-} ->
+file { '/var/www/error/404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
 
 file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => $nginx_conf
-} ->
+  ensure  => present,
+  content => "server {
+    listen 80;
+    listen [::]:80 default_server;
+
+    add_header X-Served-By \$hostname;
+
+	  root /var/www/html/;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+	  location /hbnb_static/ {
+        alias /data/web_static/current/;
+	}
+
+    error_page 404 /404.html;
+    location = /404.html {
+        root /var/www/error/;
+        internal;
+    }
+}",
+}
 
 exec { 'nginx restart':
-  path => '/etc/init.d/'
+  path => '/etc/init.d/',
 }
