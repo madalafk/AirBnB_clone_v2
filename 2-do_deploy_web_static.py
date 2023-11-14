dfcfe66b0023ef0818fe0767a116ec914f01de14
madalafk <fmadala@hotmail.com>
2023-11-14 11:56:52 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
 """
-A PY script to distributes a .tgz archive using Fabric.
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
-import os
-from datetime import datetime
-from fabric.api import local, env, put, run
 
-"""
-my Remote webservers details
-"""
-env.hosts = ["100.25.30.112", "54.237.113.234"]
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/school"
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['100.25.30.112', '54.237.113.234']
 
-def do_pack():
-    """
-    Generates a .tgz archive from the contents of web_static folder.
-    """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-        return output
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
